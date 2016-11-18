7874fe11392c2e4723ee06ba84d054a18ba01d27
ggionx <fred@frederickcheung.com>
2016-11-17 19:21:22 -0500

    MCA model graph output

--- Fred/RidgeRegression.R
+++ Fred/RidgeRegression.R
@@ -15,9 +15,12 @@ y <- cont.var$loss
 x_test <- x[test.index,]
 y_test <- y[test.index]
 x_train <- x[train.index,]
-y_train <- y[train.index]
+y_train <- log(y[train.index])
 
-grid <- 10^seq(5, -2, length = 100)
+grid <- 10^seq(-10, -20, length = 100)
 ridge_train <- glmnet(x_train, y_train, alpha = 0, lambda = grid)
 
 plot(ridge_train)
+###continuous show that they do not have any correlation to the loss variable
+###we conclude that most of the continuous variables will not contribute to our model
+###for any linear regression  model, we will exclude them
